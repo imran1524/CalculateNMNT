f161c90b62a756979dabbe9e159cb0219286befe
Imran Khan <ikhan@imrans-mbp.home>
2021-01-04 15:59:10 +0000

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/CalculateNMNT.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CalculateNMNT.dir/main.cpp.o"
+  "CalculateNMNT"
+  "CalculateNMNT.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CalculateNMNT.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
